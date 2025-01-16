e5215426fb228b5a030dcd1b609064ede87ad000
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-01-16 16:16:33 +0530

    Revert "next config commit"
    This reverts commit 2431b47fb718ad6c155fb0fad9ba011eaf4dcb26.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/',  // This is correct for GitHub Pages deployment
     images: {
       unoptimized: true,
     },
