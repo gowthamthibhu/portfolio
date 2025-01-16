fdc8b17c1c947d6ffe2fc79b61062207e9fe3f47
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-01-16 14:22:18 +0530

    next config commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/portfolio-deploy',  // This is correct for GitHub Pages deployment
+    basePath: '/',  // This is correct for GitHub Pages deployment
     images: {
       unoptimized: true,
     },
